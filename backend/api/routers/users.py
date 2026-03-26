56b103e462472d9b02141ae981428753d0b84fcb
gustavocosta <gustavotcosta79@gmail.com>
2026-03-26 14:27:44 +0000

    backend feito (????)

--- backend/api/routers/users.py
+++ backend/api/routers/users.py
@@ -2,16 +2,15 @@ from typing import List
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from api.deps import get_db
-from models.activity import ActivityResponse, ActivityCreate
+from models.activity import ActivityResponse
 from models.devscore import DevScoreResponse
-from models.repository import RepositoryResponse, RepositoryUpdate, RepositoryCreate
+from models.repository import RepositoryResponse
 from models.user import UserResponse, UserCreate, UserUpdate
 from services.activity_service import ActivityService
 from services.devscore_service import DevscoreService
 from services.repository_service import RepositoryService
 from services.user_service import UserService
-from services.github_service import GitHubService
-
+from models.dashboard import DashboardResponse
 router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/", response_model=UserResponse)
@@ -25,87 +24,9 @@ def register_user (user: UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/import/{github_username}", response_model=UserResponse)
 def import_user_from_github (github_username: str,db: Session = Depends(get_db)):
-    github_service = GitHubService()
-    user_service = UserService(db)
-    repo_service = RepositoryService(db)
-    activity_service = ActivityService (db)
+    service = UserService(db)
 
-    github_data = github_service.get_user_profile(github_username)
-
-    if not github_data:
-        raise HTTPException(status_code=404, detail="Este username não existe no GitHub!")
-
-    existing_user = user_service.get_by_github_id(github_data['id'])
-    if existing_user:
-        db_user = existing_user
-    else:
-        github_email = github_data.get("email")
-        if not github_email:
-            github_email = f"{github_data['login']}@users.noreply.github.com"
-
-        new_user = UserCreate (
-            github_id=github_data['id'],
-            username=github_data['login'],
-            email=github_email,
-            avatar_url=github_data.get("avatar_url")
-        )
-
-        db_user = user_service.create_user(new_user)
-
-    # Sincronizar repositórios
-    repos_data = github_service.get_user_repos(github_username)
-
-    for repo in repos_data:
-        existing_repo = repo_service.get_by_github_repo_id(repo["id"])
-
-        repo_size = repo.get("size",0)
-        repo_complexity = repo_service.calculate_complexity(repo_size)
-
-        if existing_repo:
-            repo_update = RepositoryUpdate (
-                name=repo["name"],
-                language=repo.get("language"),
-                stars_count=repo.get("stargazers_count", 0),
-                complexity = repo_complexity
-            )
-            repo_service.update(existing_repo.id,repo_update)
-        else:
-            new_repo = RepositoryCreate(
-                github_repo_id = repo["id"],
-                name = repo["name"],
-                language =  repo.get("language"),  # porque a linguagem pode ser nula
-                stars_count = repo.get ("stargazers_count"),
-                owner_id = db_user.id,
-                created_at = repo["created_at"],
-                complexity = repo_complexity
-
-            )
-            repo_service.create(new_repo)
-
-    #SINCRONIZAR ACTIVITIES
-
-    activities_data = github_service.get_user_activities(github_username)
-
-    for event in activities_data:
-        github_repo_id = event["repo"]["id"]
-
-        existing_repo = repo_service.get_by_github_repo_id(github_repo_id)
-        if existing_repo:
-            new_activity = ActivityCreate (
-                type=event["type"],
-                repo_id=existing_repo.id,
-                user_id=db_user.id,
-                created_at=event["created_at"]
-            )
-            activity_service.create(new_activity)
-
-    # CALCULAR O DEVSCORE
-    devscore_service = DevscoreService (db)
-    devscore_service.calculate_devscore_for_user(db_user.id)
-
-    db.refresh(db_user)
-
-    return db_user
+    return service.sync_user_from_github(github_username)
 
 @router.get("/github/{github_id}", response_model= UserResponse)
 def get_user_by_github_id (github_id : int, db: Session = Depends(get_db)):
@@ -149,6 +70,17 @@ def get_devscore_by_user_id (user_id:int, db: Session = Depends(get_db)):
     if db_devscore is None:
         raise HTTPException(status_code=404, detail="Devscore não encontrado para este utilizador")
     return db_devscore
+
+
+@router.get("/{user_id}/dashboard", response_model=DashboardResponse)
+def get_dashboard_data(user_id: int, db: Session = Depends(get_db)):
+    service = UserService(db)
+
+    dashboard_data = service.get_user_dashboard(user_id)
+    if not dashboard_data:
+        raise HTTPException(status_code=404, detail="Utilizador não encontrado")
+
+    return dashboard_data
 
 @router.put("/{user_id}",response_model=UserResponse)
 def update_user (user_id:int, user_update: UserUpdate, db: Session = Depends(get_db)):
---
+++ backend/models/dashboard.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import Dict,Any,List
+
+class DashboardResponse (BaseModel):
+    user_info : Dict[str,Any]
+
+    total_repos:int
+    total_stars:int
+    current_devscore:float
+
+    commits_per_month : Dict[str,int] # Ex: {"2023-10": 15, "2023-11": 42}
+    devscore_evolution :Dict[str,float] # Ex: {"2023-10-25": 85.0, "2023-10-26": 92.5}
+    language_evolution : Dict[str,Dict[str,int]] # Ex: {"2023": {"Python": 2}, "2024": {"Python": 5, "C": 1}}
+    projects_over_time : Dict[str,int]
+
