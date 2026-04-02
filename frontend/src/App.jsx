fb60d6334ab36e9c63a76627fb1bf8de45182728
Gustavo Costa <gustavotcosta79@gmail.com>
2026-04-02 14:53:16 +0100

    Criação do frontend (projeto em react + tailwind css)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+    return (
+        <main className={'min-h-screen p-8'}>
+          <h1 className="text-4xl font-bold text-accent text-center mt-10">
+            DevTrack frontend!
+          </h1>
+        </main>
+    )
+}
+
+export default App
